859805bae6488522e98612ac1a447b0c37504baf
Faiq Malik <faiqmalik2@gmail.com>
2020-11-18 11:19:23 -0500

    fixed proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    "api",
+    "/api",
     createProxyMiddleware({
       target: "http://localhost:1337",
       changeOrigin: true,
